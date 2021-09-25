ca16f3f8d44c1c363ddaf8f8f70819fec5f260b2
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-25 22:22:26 +0800

    Update tic_tac_toe.py

--- codeitsuisse/routes/tic_tac_toe.py
+++ codeitsuisse/routes/tic_tac_toe.py
@@ -15,9 +15,10 @@ def tic_tac_toe():
     logging.info("data sent for evaluation {}".format(data))
     id = data.get("battleId")
     url = 'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/' + id
-    data = requests.get(url)
+    data_1 = requests.get(url)
+    logging.info("data sent for evaluation {}".format(data_1))
     a = data.get("youAre")
-
+    logging.info("You are {}".format(a))
     return 0
 
 
