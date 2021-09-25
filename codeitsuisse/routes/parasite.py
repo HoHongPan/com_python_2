3936baabfc064d443698be2f8d2a1156cdf9cc51
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-26 00:03:11 +0800

    Update parasite.py

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -8,16 +8,40 @@ from codeitsuisse import app
 
 logger = logging.getLogger(__name__)
 
+
 @app.route('/parasite', methods=['POST'])
 def parasite():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-    for i in range(20):
-        result = request.get_json()[i].get("room")
-        logging.info("data sent for evaluation {}".format(result))
+    result = "[\n"
+    for i in range(2):
+        result = result + "  {\n"
+        c = request.get_json()[i].get("room")
+        logging.info("data sent for evaluation {}".format(c))
+        result = result + "    \"room\": " + str(c) + ",\n"
         a = request.get_json()[i].get("grid")
         logging.info("data sent for evaluation {}".format(a))
+        rows_a = len(a)
+        columns_a = len(a[0])
         b = request.get_json()[i].get("interestedIndividuals")
         logging.info("data sent for evaluation {}".format(b))
-    return "result"
+        rows_b = len(b)
+        result = result + "    \"p1\": { "
+        for n in range(rows_b):
+            result = result +"\""+ b[n] + "\":  "
+            if int(a[int(b[n][0])][int(b[n][2])]) != 1:
+                result = result + "-1"
+            else:
+                result = result + "0"
+            if n < rows_b-1:
+                result = result + ", "
+        else:
+            result = result + "},\n "
+
+        result = result + "  }\n"
+    else:
+        result = result + "]"
+        print (result)
+
+    return result
 
