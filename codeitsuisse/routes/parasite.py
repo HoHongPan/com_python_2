c0158e2aedb0d60553b7a53669f496cca89337d3
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-25 22:47:00 +0800

    Update parasite.py

--- codeitsuisse/routes/parasite.py
+++ codeitsuisse/routes/parasite.py
@@ -9,10 +9,17 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 
-@app.route('/parasite', methods=['POST'])
-def parasite():
+@app.route('/tic-tac-toe', methods=['POST'])
+def tic_tac_toe():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
-
+    id = data.get("battleId")
+    ab = 'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/' + id
+    data_1 = requests.get(ab)
+    logging.info("data sent for evaluation {}".format(data_1))
+    a = data.get("youAre")
+    logging.info("You are {}".format(a))
     return 0
+
+
 
