2514e35a40e75eca6c78cb029fecea3f19199837
Jackyho <79627347+HoHongPan@users.noreply.github.com>
2021-09-25 20:26:15 +0800

    Create tic_tac_toe.py

---
+++ codeitsuisse/routes/tic_tac_toe.py
@@ -0,0 +1,29 @@
+import logging
+import json
+import sseclient
+import pprint
+
+from flask import request, jsonify
+
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+def with_urllib3(url):
+    import urllib3
+    http = urllib3.PoolManager()
+    return http.request('GET', url, preload_content=False)
+
+
+@app.route('/tic-tac-toe', methods=['POST'])
+def tic_tac_toe():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    id = data.get("battleId")
+    url = 'https://cis2021-arena.herokuapp.com/tic-tac-toe/start/' + id
+    logging.info("uri {}".format(url))
+    response = with_urllib3(url)
+    client = sseclint.SSEClient(respone)
+    for event in client.event():
+        pprint.pprint(json.loads(event.data))
+    return json.dumps(url)
